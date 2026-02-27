a2385d8d6fbacc3671999248ceb8c596fedf5543
ZoIQ <rusya.petrov47@gmail.com>
2026-02-26 20:25:41 -0800

    now used only django-filter

--- activities/filters.py
+++ activities/filters.py
@@ -1,18 +1,26 @@
-"""FilterSet definitions for activity listing queries."""
-
-import django_filters
-
-from activities.models import Activity
+import django_filters 
+from activities.models import Activity, ActivityType
 
 
 class ActivityFilterSet(django_filters.FilterSet):
     """Filter activities by exact fields plus activity name text search."""
 
-    type = django_filters.CharFilter(field_name="type", lookup_expr="exact")
+    type = django_filters.ChoiceFilter(
+        field_name="type",
+        choices=ActivityType.choices,
+        empty_label="Any"
+    )
     participants = django_filters.NumberFilter(field_name="participants")
     price = django_filters.NumberFilter(field_name="price")
-    accessibility = django_filters.CharFilter(field_name="accessibility", lookup_expr="exact")
-    activity = django_filters.CharFilter(field_name="activity", lookup_expr="icontains")
+    accessibility = django_filters.ChoiceFilter(
+        field_name="accessibility",
+        empty_label="Any"
+    )
+    activity = django_filters.CharFilter(
+        field_name="activity",
+        lookup_expr="icontains",
+        label="Activity name"
+    )
 
     class Meta:
         model = Activity
